c32cf4aa4a027020968868c729b109e1275a68d5
adriabama06 <adriabama06@gmail.com>
2022-05-19 22:32:30 +0200

    add multithread for Go

---
+++ Go/multithread/mainAndThread.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func print(message string, who string) {
+	for i := 0; i < 5; i++ { // print 5 times the message
+		time.Sleep(100 * time.Millisecond) // sleep 100ms
+		fmt.Printf("%s: %s\n", who, message)
+	}
+}
+
+func main() {
+	go print("Hello World", "At Go") // this will run in a separate thread, and will print "Hello World" 5 times
+	print("Hello World", "Main")
+	//time.Sleep(600 * time.Millisecond) // sleep 600ms
+	// why wait, this is not runing at main thread, because this dont affect at main thread
+
+}
