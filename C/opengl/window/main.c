c11fcc61d75e183abbf9920986d0c1a7256114e9
adriabama06 <adriabama06@gmail.com>
2022-06-23 13:03:56 +0200

    add opengl for windows + test

---
+++ C/opengl/window/main.c
@@ -0,0 +1,44 @@
+// https://www.absingh.com/opengl/
+// http://www.opengl-tutorial.org/es/beginners-tutorials/tutorial-1-opening-a-window/
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glut.h>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_POINTS);
+	glVertex2f(10.0, 10.0);
+	glVertex2f(150.0, 80.0);
+	glVertex2f(100.0, 20.0);
+	glVertex2f(200.0, 100.0);
+	glEnd();
+	glFlush();
+}
+
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
+
+	myinit();
+	glutMainLoop();
+    
+    return 0;
+}
