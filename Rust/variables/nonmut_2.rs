c88e675bab4e7981c5ed62417759f64500a7f770
adriabama06 <adriabama06@gmail.com>
2024-02-25 12:29:59 +0100

    Rust mutable variables

---
+++ Rust/variables/nonmut_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x;
+
+    println!("Hello World");
+
+    x = 5;
+
+    println!("x: {x}");
+}
---
+++ Rust/variables/nonmut_3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 5;
+
+    println!("x: {x}");
+
+    x = 22;
+
+    println!("x: {x}");
+}
